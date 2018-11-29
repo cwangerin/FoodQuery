02fd36af2f2ce27e22bf0e4bd8a7a83df5657382
bernard <brufinus@wisc.edu>
2018-11-29 15:55:39 -0600

    Updated Main.java

--- Main.java
+++ Main.java
@@ -15,11 +15,15 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			BorderPane root = new BorderPane();
-			Scene scene = new Scene(root,400,400);
+			BorderPane base = new BorderPane();
+			
+			Scene scene = new Scene(base,1280,720); // Standard 16:9 aspect ratio
+			
+			// Sets and shows the stage. DO NOT EDIT UNLESS NECESSARY
 			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
 			primaryStage.setScene(scene);
 			primaryStage.show();
+			
 		} catch(Exception e) {
 			e.printStackTrace();
 		}
