0a69f6d0b3bcd585c861f38f3fac4d420dc14218
bernard <brufinus@wisc.edu>
2018-11-29 12:17:41 -0600

    Added JavaFX package

---
+++ application/Main.java
@@ -0,0 +1,38 @@
+package application;
+	
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+
+
+public class Main extends Application {
+	// Fields
+	
+	/**
+	 * For building UI controls, layouts, scenes, stage, etc.
+	 */
+	@Override
+	public void start(Stage primaryStage) {
+		try {
+			BorderPane root = new BorderPane();
+			Scene scene = new Scene(root,400,400);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			primaryStage.setScene(scene);
+			primaryStage.show();
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * For processing arguments
+	 * 
+	 * Used to read data file, set username, permissions, call init(agrs), etc.
+	 * 
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
